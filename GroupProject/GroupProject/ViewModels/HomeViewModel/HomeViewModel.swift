eb3dcadff0ad3168723161bb7b60bd6700877f53
minh0811 <vokhaiminh0811@gmail.com>
2023-09-16 14:16:15 +0700

    Add ViewModel for some files and refactor

---
+++ GroupProject/GroupProject/ViewModels/HomeViewModel/HomeViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewModel.swift
+//  GroupProject
+//
+//  Created by Minh Vo on 16/09/2023.
+//
+
+import SwiftUI
+
+struct HomeViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewModel()
+    }
+}
